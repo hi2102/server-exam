1c0057523b145f0cc339ef9d2210595ff63ccd29
hi2102 <hi2102@naver.com>
2023-04-18 12:25:56 +0900

    add pkg

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,29 @@
-import * as express from 'express';
 import * as bodyParser from 'body-parser';
- 
-function loggerMiddleware(req: express.Request, res: express.Response, next) {
-  console.log(`${req.method} ${req.path}`);
-  next();
+import { Request, Response, NextFunction } from 'express';
+// import { RequestHandler } from "express";
+// export const 메서드: RequestHandler = (req, res, next) => {};
+
+function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
+	console.log(`${req.method} ${req.path}`);
+	next();
 }
 
-const express = require('express')
-const app = express()
-const PORT = 4000
+const express = require('express');
+const app = express();
+const PORT = 4000;
 
-app.use(loggerMiddleware)
+app.use(loggerMiddleware);
 app.use(bodyParser.json());
 
+app.get('/', (req: Request, res: Response) => {
+	res.send(req.body);
+});
 
-app.get('/', (req, res) => {
-  res.send(req.body)
-})
-app.get('/hello', (req, res) => {
-  res.send('hello, world!')
-})
+// POST method route
+app.post('/', function (req: Request, res: Response) {
+	res.send('POST request to the homepage');
+});
 
 app.listen(PORT, () => {
-  console.log(`app listening on ${PORT}`)
-})
+	console.log(`app listening on ${PORT}`);
+});
